7ab41bf857e7a175388a4fada4b300550787a865
breguenice <breguenicedev@gmail.com>
2024-07-22 23:23:49 -0300

    iniciando estilização da primeira section

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,74 +1,76 @@
 import styled from "styled-components"
+import Header from "../components/header/header"
 
 export default function Homepage() {
     return (
         <div>
-            <Header>
-                <LogoBookmark>
 
-                </LogoBookmark>
+            <Header />
+
+            <FirstSection>
                 <div>
-                    <ul>
-                        <li>FEATURES</li>
-                        <li>PRICING</li>
-                        <li>CONTACT</li>
-                        <button>LOGIN</button>
-                    </ul>
+                    <h1>A Simple Bookmark Manager</h1>
+                    <p>A clean and simple interface to organize your favourite websites. Open a new
+                        browser tab and see your sites load instantly. Try it for free.
+                    </p>
+                    <ButtonContainer>
+                        <BlueButton>Get it on Chrome</BlueButton>
+                        <GrayButton>Get it on Chrome</GrayButton>
+                    </ButtonContainer>
                 </div>
-            </Header>
-
-            <section>
-                a
-            </section>
+            </FirstSection>
         </div>
     )
 }
 
-    const LogoBookmark = styled.div`
-    width: 300px;
-    height: 30px;
-    background-image: url('../../images/logo-bookmark.svg');
-    background-repeat: no-repeat;
-    `
-    const Header = styled.header`
-        width: 100vw;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        padding: 60px 100px;
 
-        &   ul{
-            display: flex;
-            justify-content: space-evenly;
-            width: 500px;
-            list-style: none;
-            font-family: "Rubik", sans-serif;
-            font-weight: 300;
-            color: hsl(229, 8%, 60%);
-            font-size: 18px;
-            align-items: center;
-    
-            & li{
-                cursor: pointer;
-            }
+const ButtonContainer = styled.div`
+    display: flex;
+    gap: 20px;
+    width: auto;
+    height: auto;
+`
 
-            & li:hover{
-                color: hsl(0, 94%, 66%);
-            }
+const BlueButton = styled.button`
+    width: 150px;
+    padding: 10px 20px;
+    border: 2px solid transparent;
+    cursor: pointer;
+    background-color: hsl(231, 69%, 60%);
+    color: white;
+    border-radius: 5px;
+    font-weight: 400;
+    &:hover{
+        border: 2px solid hsl(231, 69%, 60%);
+        background-color: transparent;
+        color: hsl(231, 69%, 60%);
+    }
+`
 
-            & button{
-                background-color: hsl(0, 94%, 66%);
-                padding: 10px 20px;
-                border-radius: 5px;
-                color: white;
-                border: 2px solid transparent;
-            }
+const GrayButton = styled.button`
+    width: 150px;
+    padding: 10px 20px;
+    border: 2px solid transparent;
+    cursor: pointer;
+    background-color: transparent;
+    color: black;
+    border-radius: 5px;
+    box-shadow: 0px 0px 6px black;
+    font-weight: 400;
+    &:hover{
+        border: 2px solid black;
+        background-color: transparent;
+        color: #000000c8;
+        box-shadow: none;
+    }
+`
 
-            & button:hover{
-                background-color: white;
-                color: hsl(0, 94%, 66%);
-                cursor: pointer;
-                border: 2px solid hsl(0, 94%, 66%);
-            }
-        }
-    `
+const FirstSection = styled.section`
+    & h1{
+        font-size: 50px;
+    }
+
+    & p{
+        color: hsl(229, 8%, 60%);
+    }
+`
